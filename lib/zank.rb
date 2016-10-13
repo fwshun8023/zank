01debf995c1662fd28c0548069f4f355a4f636d0
fanwei <fwshun8023@gmail.com>
2016-10-13 20:29:39 +0800

    posts

--- lib/zank.rb
+++ lib/zank.rb
@@ -5,11 +5,13 @@ require 'zank/version'
 require 'zank/client'
 require 'zank/user'
 require 'zank/circle'
+require 'zank/post'
 require 'faraday'
 
 module Zank
   BASE_URL     = 'http://apiproxy.zank.mobi'.freeze
   LOGIN_PATH   = '/snowball/api/account/account/login.json'.freeze
   LOGOUT_PATH  = '/snowball/api/account/account/logout.json'.freeze
-  CIRCLES_PATH = '/snowball/api/circle/circle/queryMyCircle.json'.freeze
+  CIRCLES_PATH = '/snowball/api/circle/circle/queryMyJoinCircle.json'.freeze
+  POSTS_PATH   = '/snowball/api/circle/circle/queryPostByCircleId.json'.freeze
 end
